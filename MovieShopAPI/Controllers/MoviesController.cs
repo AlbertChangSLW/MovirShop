a3518e2ca575c07e9d81b7c818c65bc9be94aa2a
YaoChung Chang <AlbertChangSLW@hotmail.com>
2022-05-25 23:06:21 -0600

    add the code from the 0524 class

---
+++ MovieShopAPI/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        [Route("top-grossing")]
+        [HttpGet]
+        public async Task<IActionResult> TopGrossing()
+        {
+            var movies = await _movieService.GetTop30GrossingMovies();
+
+            if (movies == null || !movies.Any())
+            {
+                return NotFound(new { errorMessage = "No Movies found" });
+            }
+            return Ok(movies);
+        }
+
+        [Route("(id)")]
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var movies = await _movieService.GetMovieDetails(id);
+
+            if(movies == null)
+            {
+                return NotFound(new { errorMessage = "No Movies found" });
+            }
+            return Ok(movies);
+        }
+
+    }
+}
